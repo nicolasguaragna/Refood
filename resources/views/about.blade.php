db55c1ad20a58097f10ba79471835bc7e2502fc6
nicolasguaragna <fernando.guaragna@davinci.edu.ar>
2024-09-29 18:56:46 -0300

    ok

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,52 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<x-layout>
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Quiénes Somos</title>
-
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/styles.css">
-</head>
-
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="home">Refood</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                    <ul class="navbar-nav">
-                    <li class="nav-item">
-                            <a class="nav-link" href="<?= url('/');?>">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="<?= url('servicios');?>">Servicios</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="<?= url('blog');?>">Blog</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="<?= url('quienes-somos');?>">Quienes Somos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="<?= url('contacto');?>">Contacto</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <main class="container p-4">
-            <h1 class="mb-3">Quiénes Somos</h1>
-        </main>
-        <footer class="footer">
-            <p>Copyright &copy; Da vinci 2024</p>
-        </footer>
-    </div>
-</body>
-
-</html>
+    <x-slot:title>Quiénes Somos</x-slot>
+    <h1 class="mb-3">Quiénes Somos</h1>
+</x-layout>
