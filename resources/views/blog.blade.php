a1f7dcc9719c96c5e8ce841e78b23548c49d31dd
nicolasguaragna <fernando.guaragna@davinci.edu.ar>
2024-09-29 13:26:09 -0300

    ok

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Blog</title>
 
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/styles.css">
