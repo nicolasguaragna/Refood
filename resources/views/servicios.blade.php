b41edd5c1072bab5b1e2ad9a736545e37fd7aa4c
nicolasguaragna <fernando.guaragna@davinci.edu.ar>
2024-10-03 00:29:36 -0300

    update

--- resources/views/servicios.blade.php
+++ resources/views/servicios.blade.php
@@ -1,11 +1,21 @@
 <x-layout>
     <x-slot:title>Nuestros Servicios</x-slot:title>
     
-    <h1 class="mb-3">Nuestros Servicios</h1>
-    
-    <ul>
-        @foreach ($services as $service)
-            <li>{{ $service->name }} - Precio: ${{ number_format($service->price, 2) }}</li>
-        @endforeach
-    </ul>
+    <div class="container mt-5">
+        <h1 class="text-center mb-4">Nuestros Servicios</h1>
+        <div class="row">
+            @foreach ($services as $service)
+                <div class="col-md-4">
+                    <div class="card mb-4 shadow-sm">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $service->name }}</h5>
+                            <p class="card-text">{{ $service->description }}</p>
+                            <p class="card-text"><strong>Precio:</strong> ${{ number_format($service->price, 2) }}</p>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 </x-layout>
+
