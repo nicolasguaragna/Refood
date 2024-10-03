c8fff54f25b2a4da4bc5eca3115eb83686909dfe
nicolasguaragna <fernando.guaragna@davinci.edu.ar>
2024-10-03 00:08:45 -0300

    ok

--- app/Http/Controllers/ServiciosController.php
+++ app/Http/Controllers/ServiciosController.php
@@ -2,11 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Service;
 use Illuminate\Http\Request;
 
 class ServiciosController extends Controller
 {
-    public function index() {
+    public function index() 
+    {
+        /*traigo los registros de la tabla services con eloquent.*/
+
+        $services = Service::all(); // Obtener todos los servicios
+
+        //dump and die.
+        dd($services);
+
         return view('servicios');
     }
 }
