b55f0654912673649e2a664d6d1ffcdce5c727ed
nicolasguaragna <fernando.guaragna@davinci.edu.ar>
2024-11-10 19:37:39 -0300

    update

--- resources/views/rescues/rescue.blade.php
+++ resources/views/rescues/rescue.blade.php
@@ -22,6 +22,25 @@
             <!-- Campo oculto para el ID del servicio -->
             <input type="hidden" name="service_id" value="{{ $service->service_id }}">
 
+            <!-- Campo para el nombre -->
+            <div class="mb-3">
+                <label for="name" class="form-label">Nombre</label>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Tu nombre" required value="{{ old('name') }}">
+            </div>
+
+            <!-- Campo para el contacto -->
+            <div class="mb-3">
+                <label for="contact" class="form-label">Contacto</label>
+                <input type="text" class="form-control" id="contact" name="contact" placeholder="Teléfono o correo de contacto" required value="{{ old('contact') }}">
+            </div>
+
+            <!-- Campo para la ubicación -->
+            <div class="mb-3">
+                <label for="location" class="form-label">Ubicación</label>
+                <input type="text" class="form-control" id="location" name="location" placeholder="Direccion del rescate" required value="{{ old('location') }}">
+            </div>
+
+            <!-- Campo para los detalles del rescate -->
             <div class="mb-3">
                 <label for="details" class="form-label">Detalles del Rescate</label>
                 <textarea class="form-control" id="details" name="details" rows="4" placeholder="Describe el rescate que necesitas..." required>{{ old('details') }}</textarea>
