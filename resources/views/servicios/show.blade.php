888fdef53a074e4682c584fb784d8f9a3e62366d
nicolasguaragna <fernando.guaragna@davinci.edu.ar>
2024-11-14 23:01:17 -0300

    update

--- resources/views/servicios/show.blade.php
+++ resources/views/servicios/show.blade.php
@@ -1,13 +1,13 @@
 <x-layout>
     <x-slot:title>{{ $service->name }}</x-slot:title>
-    
-    <div class="container mt-5">
-        <h1>{{ $service->name }}</h1>
-        <p>{{ $service->description }}</p>
-        <p><strong>Precio:</strong> ${{ number_format($service->price, 2) }}</p>
-        <p><strong>Mínimo de Kg:</strong> {{ $service->minimum_kg }} kg</p>
 
-        <h2 class="mb-3">Mas info</h2>
-        <div>{{ $service->more_info }}</div>
+    <div class="service-container mt-5">
+        <h1 class="service-title">{{ $service->name }}</h1>
+        <h2 class="service-subtitle">{{ $service->description }}</h2>
+        <p class="service-price"><strong>Precio:</strong> ${{ number_format($service->price, 2) }}</p>
+        <p class="service-price"><strong>Mínimo de Kg:</strong> {{ $service->minimum_kg }} kg</p>
+
+        <h3 class="service-subtitle">Más info</h3>
+        <p class="service-info">{{ $service->more_info }}</p>
     </div>
 </x-layout>
