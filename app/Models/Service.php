eed84232007c861201c17820a3b4a3252c9c7fd8
nicolasguaragna <fernando.guaragna@davinci.edu.ar>
2024-12-19 18:53:55 -0300

    update

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -2,68 +2,14 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class Service extends Model
 {
-    use HasFactory, Notifiable;
+    use HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
-
-    /**
-     * Define la relación de muchos a muchos con el modelo Rol.
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
-     */
-    public function roles()
-    {
-        return $this->belongsToMany(Rol::class, 'rol_user');
-    }
-
-    // Método para verificar si el usuario tiene un rol específico
-    public function hasRole($roleName)
-    {
-        return $this->roles()->where('name', $roleName)->exists();
-    }
-
-    // Relación con las solicitudes de rescate
-    public function rescueRequests()
-    {
-        return $this->hasMany(RescueRequest::class, 'user_id');
-    }
+    protected $primaryKey = "service_id";
+    public $incrementing = true;
+    protected $keyType = 'int';
 }
